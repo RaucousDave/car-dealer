3c4090de9bf8fdc407b714fa5d58ddab38254f1f
Davies Enienghan <daviesenienghan906@gmail.com>
2025-05-25 15:13:36 +0100

    Fixed some responsive issues

--- src/components/about/WhyChooseUs.jsx
+++ src/components/about/WhyChooseUs.jsx
@@ -31,7 +31,7 @@ export default function WhyChooseUs() {
           We’re Not Just Selling Cars. We’re Curating Experiences.
         </p>
         <div className="flex px-5 flex-col-reverse md:flex-row gap-4 md:gap-0 py-8">
-          <div className="flex flex-col gap-7 px-3 w-[100%] md:w-[70%] justify-center items-center">
+          <div className="flex flex-col gap-7 px-3  md:w-[70%] justify-center items-center">
             {reasons.map((reason, index) => {
               const Icon = reason.icon;
               return (
